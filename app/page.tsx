982ab1018ed32369fb8a48b52de1b3544092d679
guijiangheng <guijiangheng@bytedance.com>
2023-08-01 11:52:32 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div>
+      <div data-test="test">test</div>
+      <style>{`[data-test="test"] { background: red; }`}</style>
+    </div>
+  );
 }
